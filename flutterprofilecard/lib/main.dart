c0a0d3906db6bbe17edd6829e2bddc753d4aec22
Jason <ironelder@gmail.com>
2019-10-20 14:28:33 +0900

    container test

--- flutterprofilecard/lib/main.dart
+++ flutterprofilecard/lib/main.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(MyProfile());
+}
+
+class MyProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.blueGrey,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.redAccent,
+                child: Text('Container 3'),
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                color: Colors.lightBlue,
+                child: Text('Container 4'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
