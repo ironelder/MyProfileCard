ce20c51220739a0faeeaf155de0b81d268ee1ad1
Jason <ironelder@gmail.com>
2019-10-20 15:40:16 +0900

    MyProfile First Make

--- flutterprofilecard/lib/main.dart
+++ flutterprofilecard/lib/main.dart
@@ -12,33 +12,72 @@ class MyProfile extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/MyProfile.jpg'),
+              ),
+              Text(
+                'ironelder',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 4.5,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.teal.shade100,
+                    fontSize: 20.0,
+                    letterSpacing: 5.5,
+                    fontWeight: FontWeight.bold),
+              ),
               Container(
-                height: 100.0,
-                width: 100.0,
+                padding: EdgeInsets.all(10.0),
                 color: Colors.white,
-                child: Text('Container 1'),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                      size: 30.0,
+                    ),
+                    Text(
+                      '+82 10 4332 4029',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    )
+                  ],
+                ),
               ),
-              SizedBox(height: 20.0,),
               Container(
-                height: 100.0,
-                width: double.infinity,
-                color: Colors.blueGrey,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100.0,
-                width: double.infinity,
-                color: Colors.redAccent,
-                child: Text('Container 3'),
-              ),
-              Container(
-                height: 100.0,
-                width: double.infinity,
-                color: Colors.lightBlue,
-                child: Text('Container 4'),
-              ),
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                      size: 30.0,
+                    ),
+                    Text(
+                      'ironelder@gmail.com',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    )
+                  ],
+                ),
+              )
             ],
           ),
         ),
